3d6bf72ebae83d06e2b06e739d54b0b9b7f90d60
Ekrem <ekrem@3pounds.io>
2021-12-24 14:57:46 +0300

    First Commit good commit :)

---
+++ FaceIDApp/FaceIDApp/FaceIDAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceIDAppApp.swift
+//  FaceIDApp
+//
+//  Created by Ekrem Özkaraca on 24.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct FaceIDAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
